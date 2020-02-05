0377bb5b0e1d5511a3e1be03534ba3801ff81617
Jun Yang <JYang@ucvts.tec.nj.us>
2020-02-05 10:16:58 -0500

    こんにちは(https://youtu.be/dQ_d_VKrFgM)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-
+///////////////////// CONSTANTS /////////////////////////////////////
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7 ,8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let board;
+let turn;
+let win;
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
+const start = document.getElementById("start");
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+document.getElementById("board").onclick= takeTurn;
+document.getElementById("reset-button").onclick = init;
+///////////////////// FUNCTIONS /////////////////////////////////////
+function init() {
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+}

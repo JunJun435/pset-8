6fe925911e73b302a83cf39ec32d918ad631ab1a
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-02-09 19:39:04 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -34,4 +34,67 @@ function init() {
     "", "", "",
     "", "", ""
   ];
+  turn = "X";
+  win = null;
+
+  render();
+}
+
+function firstX() {
+  document.getElementById('turnButton').innerHTML = "Turn: X";
+  turn = "X";
+}
+
+function firstO() {
+  document.getElementById('turnButton').innerHTML = "Turn: O";
+  turn = "O";
+}
+
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
+  });
+
+  message.textContent =
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+}
+
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    if (board[index] === "") {
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
+    }
+  }
+}
+
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+  if (winner === "X") {
+    keepScoreX++;
+    document.getElementById('ScoreX').innerHTML = keepScoreX;
+  } else if (winner === "O") {
+    keepScoreO++;
+    document.getElementById('ScoreO').innerHTML = keepScoreO;
+  }
+
+  return winner ? winner : board.includes("") ? null : "T";
 }

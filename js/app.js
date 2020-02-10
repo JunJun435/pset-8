8d51d0fa2345cbc1fac85a01e2a3277671508e34
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-02-09 19:33:58 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,25 +2,31 @@
 const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
-  [6, 7 ,8],
+  [6, 7, 8],
   [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6],
-];
+  [2, 4, 6]
+]
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let board;
 let turn;
 let win;
+let keepScoreX = 0;
+let keepScoreO = 0;
+
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 const squares = Array.from(document.querySelectorAll("#board div"));
 const message = document.querySelector("h2");
-const start = document.getElementById("start");
+
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 window.onload = init;
-document.getElementById("board").onclick= takeTurn;
+document.getElementById("board").onclick = takeTurn;
 document.getElementById("reset-button").onclick = init;
+document.getElementById('ButtonX').onclick = firstX;
+document.getElementById('ButtonO').onclick = firstO;
+
 ///////////////////// FUNCTIONS /////////////////////////////////////
 function init() {
   board = [
